d5cf035ae711b915250f36ec0fdab3d2bf192f3f
lsk <1270@qq.com>
2020-12-05 16:02:26 +0800

    修改TestController

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -5,9 +5,10 @@ public class TestController {
 
     public void test(){
         System.out.println("测试");
+        //add by kk 2020.12.5
+        if(true){
+
+        }
     }
-    //add by kk 2020.12.5
-    if(true){
-        
-    }
+
 }
